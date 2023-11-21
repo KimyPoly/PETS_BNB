b29a1229276491c934706ec2dd2d83c7b3325283
Stella Ndjikessi <stelladiana@live.fr>
2023-11-21 16:13:26 +0100

    unnecessary stimulus & comments deleted

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,14 +7,7 @@ class ReviewsController < ApplicationController
     @pet = @booking.pet_id
   end
 
-  # def show
-  #   @pet = Pet.find(params[:id])
-  #   @booking = Booking.find_by(pet_id: @pet.id, user_id: current_user.id)
-  #   @review = Review.new
-  # end
-
   def create
-    # @pet = Pet.find(params[:pet_id])
     @booking = Booking.find(params[:booking_id])
     @review = Review.new(review_params)
     @review.booking = @booking
