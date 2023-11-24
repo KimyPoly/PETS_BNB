70371ddf4546c09ee67c1fcab0af62dc1054be5b
sonya bouafia <bouafiasonya@yahoo.com>
2023-11-24 12:03:26 +0100

    on a un calendrier qui est trop beau

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -1,12 +1,18 @@
 import { Controller } from "@hotwired/stimulus"
 import flatpickr from "flatpickr";
+import rangePlugin from "flatpickrRangePlugin";
 
 
 
 export default class extends Controller {
+  static targets = [ "startDate", "endDate" ]
+
   connect() {
-    flatpickr(this.element, {
+    flatpickr(this.startDateTarget, {
+      enableTime: false,
       inline: true,
-    });
+      plugins: [new rangePlugin({ input: "#endDate"})]})
+
+    flatpickr(this.endDateTarget, {})
   }
 }
